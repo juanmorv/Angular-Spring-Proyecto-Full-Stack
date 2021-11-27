801c831161115ed17d0ef423a9838eefbc1445e1
Juan María Moreno <82022153+juanmorv@users.noreply.github.com>
2021-11-27 16:27:11 +0100

    add CRUD

--- src/main/java/com/compustock/Service/ProductoService.java
+++ src/main/java/com/compustock/Service/ProductoService.java
@@ -18,5 +18,25 @@ public class ProductoService {
 	public List<Producto> findAll(){
 		return productoRepository.findAll();
 	}
+	
+	
+	public Producto findById(Long id) {
+		//Si en encuetra ese producto por su id devolvera objeto Producto 
+		//en caso de lo contrario null
+		return productoRepository.findById(id).orElse(null);
+	}
+	
+	public Producto save (Producto producto) {
+		
+		return productoRepository.save(producto);
+	}
+	
+	public void delete(Long id) {
+		
+		productoRepository.deleteById(id);
+	}
+	
 
+	
+	
 }
--- src/main/java/com/compustock/controller/ProductoController.java
+++ src/main/java/com/compustock/controller/ProductoController.java
@@ -9,7 +9,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //Cross siver para la politicas de peticiones del front end a backend
@@ -20,9 +25,37 @@ public class ProductoController {
 
 	@Autowired
 	private ProductoService productoService;
-	
+
 	@GetMapping("/productos")
 	public List<Producto> index() {
 		return productoService.findAll();
+	}
+
+	@GetMapping("/productos/{id}")
+	public Producto show(@PathVariable Long id) {
+		return productoService.findById(id);
+	}
+
+	@PostMapping("/productos") // La anotacion @RequestBody sirve para poder mapear a objeto Productos
+	public Producto create(@RequestBody Producto producto) {
+		return productoService.save(producto);
+
+	}
+
+	@PutMapping("/productos/{id}")
+	public Producto update(@RequestBody Producto producto, @PathVariable Long id) {
+		Producto productoActual = productoService.findById(id);
+
+		productoActual.setNombre(producto.getNombre());
+		productoActual.setTipo(producto.getTipo());
+		productoActual.setDescripcion(producto.getDescripcion());
+		productoActual.setPrecio(producto.getPrecio());
+
+		return productoService.save(productoActual);
+	}
+
+	@DeleteMapping("/productos/{id}")
+	public void delete(@PathVariable Long id) {
+		productoService.delete(id);
 	}
 }
